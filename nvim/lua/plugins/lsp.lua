b987eda77cab70fcc8974bc96697355e193a6586
WTFSC <72695575+xyt417@users.noreply.github.com>
2023-06-22 23:59:28 +0800

    add clangd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,3 +20,14 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("lspconfig").lua_ls.setup { -- 换成了lua_ls
   capabilities = capabilities,
 }
+
+-- clangd
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_dir = function() return vim.loop.cwd() end,
+    single_file_support = true
+})
